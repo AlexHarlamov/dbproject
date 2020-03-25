67707824b6544d83b3d29bcc79a006db749d663f
Olga <olya.zenina545@mail.ru>
2020-03-25 22:26:14 +0700

    added some tables to bd and renamed axiom to lemma

--- globals/db_config.php
+++ globals/db_config.php
@@ -1,5 +1,5 @@
 <?php
 date_default_timezone_set( "Asia/Novosibirsk" );  // http://www.php.net/manual/en/timezones.php
-define( "DB_DSN", "mysql:host=localhost;dbname=axiom_db" );
+define( "DB_DSN", "mysql:host=localhost;dbname=lemma_db" );
 define( "DB_USERNAME", "a" );
 define( "DB_PASSWORD", "a" );
