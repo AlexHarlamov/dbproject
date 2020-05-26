118e10f42b7e32f66c08bd7b232b888c3823501e
aleksey <aleksey.kharlamov.wrk@gmail.com>
2020-05-26 23:38:55 +0700

    Ru language charset error fixed

--- settings/fr_database/default.php
+++ settings/fr_database/default.php
@@ -1,6 +1,6 @@
 <?php
 
 date_default_timezone_set( "Asia/Novosibirsk" );  // http://www.php.net/manual/en/timezones.php
-define( "DB_DSN", "mysql:host=localhost;dbname=lemma_db" );
+define( "DB_DSN", "mysql:host=localhost;dbname=lemma_db;charset=utf8" );
 define( "DB_USERNAME", "root" );
 define( "DB_PASSWORD", "1234" );
