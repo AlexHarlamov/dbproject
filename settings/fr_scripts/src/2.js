a9fc6cbf70c7ed3a3d270130f11f9bdafa538d61
OlvinIR <olya.zenina545@mail.ru>
2020-05-27 14:02:04 +0700

    при создании класса возможность добавлять переменное количество атрибутов

--- settings/fr_scripts/src/2.js
+++ settings/fr_scripts/src/2.js
@@ -9,4 +9,52 @@ $(document).ready(function(){
         $(".template-preview").html(template);
     });
 
+    $(".class-edit-attr").on('click', function () {
+        let attrForm ="<div class=\"form-row\">\n" +
+            "      \n" +
+            "    <div class=\"form-group col-md-2\">\n" +
+            "      <input type=\"text\" class=\"form-control\" id=\"AttributeName#\" name=\"AttributeName#\">\n" +
+            "    </div>\n" +
+            "      \n" +
+            "    <div class=\"form-group col-md-4\">\n" +
+            "      <textarea class=\"form-control\" rows=\"1\" id=\"AttributeComment#\" placeholder=\"\" name=\"AttributeComment#\"></textarea>\n" +
+            "  </div>\n" +
+            "    \n" +
+            "    <div class=\"form-group col-md-2\">\n" +
+            "      <select id=\"AttributeType#\" class=\"form-control\" name=\"AttributeType#\">\n" +
+            "        <option selected>1</option>\n" +
+            "        <option>2</option>\n" +
+            "        <option>3</option>\n" +
+            "        <option>4</option>\n" +
+            "        <option>5</option>\n" +
+            "        <option>6</option>\n" +
+            "        <option>7</option>\n" +
+            "        <option>8</option>\n" +
+            "        <option>9</option>\n" +
+            "      </select>\n" +
+            "         </div> \n" +
+            "    \n" +
+            "    <div class=\"form-group col-md-2\">\n" +
+            "      <input type=\"number\" class=\"form-control\" id=\"AttributeSize#\" name=\"AttributeSize#\">\n" +
+            "    </div>\n" +
+            "        \n" +
+            "    <div class=\"form-group col-md-1\">\n" +
+            "      <input type=\"number\" class=\"form-control\" id=\"AttributeIndexed#\" name=\"AttributeIndexed#\">\n" +
+            "    </div>\n" +
+            "        \n" +
+            " <div class=\"form-group col-md-1\">\n" +
+            "      <select id=\"AttributeNull#\" class=\"form-control\" name=\"AttributeNull#\">\n" +
+            "        <option selected>1</option>\n" +
+            "        <option>0</option>\n" +
+            "      </select>\n" +
+            "    </div>\n" +
+            "</div>";
+
+        let length = $(".attributes").length;
+
+        console.log(length);
+        let current = attrForm.replace(/#/g,length);
+        $(".attributes").append(current);
+    });
+
 });
